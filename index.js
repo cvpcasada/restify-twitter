46a2fed1c1235419b3e50d63817ec100b22d8732
Cyrus Venn Casada <cvpcasada@outlook.com>
2016-06-02 22:26:22 +0800

    Filter un-neaded fields in search results

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var Express = require('express');
 var Twit = require('twit');
+var _ = require('lodash/core');
 
+// mixin for lodash for nested omit
+_.mixin({
+    nestedOmit: function(obj, iteratee, context) {
+        // basic _.omit on the current object
+        var r = _.omit(obj, iteratee, context);
+
+        //transform the children objects
+        _.each(r, function(val, key) {
+            if (typeof(val) === "object")
+                r[key] = _.nestedOmit(val, iteratee, context);
+        });
+
+        return r;
+    }
+});
 
 var T = new Twit({
 	consumer_key: 		'5js6h2I8ADLr1GDWfshnFAkVb',
@@ -10,16 +26,41 @@ var T = new Twit({
 });
 
 function twitterSearch(req, res) {
-	var count = (typeof req.params.count != 'undefined') ? req.params.count : 1;
+	var size = (typeof req.params.size != 'undefined') ? req.params.size : 1;
 
 	T.get('search/tweets', {
 		q: req.params.q,
-		count: count
+		size: size
 	}).catch(function (err) {
 		console.log('some error', err.stack);
 		// post a http 500
 	}).then(function(result) {
-		res.send(result.data)
+		// lets reformat result.data, retrieve only minimal values
+
+		var out = _.map(result.data.statuses, function(status) {
+			var basic = _.pick(status,[
+				'created_at',
+				'text'
+				]);
+
+			var media = _.map(status.entities.media, function(item) {
+				return _.pick(item,[
+					'media_url',
+					'type'
+					]);
+			});
+
+			var user = _.pick(status.user, [
+				'name',
+				'screen_name',
+				'location',
+				'profile_image_url'
+				]);
+
+			return _.extend(basic,{ media : media },user);
+		})
+
+		res.send(out);
 	});
 }
 
@@ -27,13 +68,12 @@ function twitterSearch(req, res) {
 var app = Express();
 app.set('port', (process.env.PORT || 3000));
 
-
 // ROUTES FOR OUR API
 // =============================================================================
 var router = Express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.route('/search/:q/:count*?').get(twitterSearch);
+router.route('/search/:q/:size*?').get(twitterSearch);
 
 // more routes for our API will happen here
 
